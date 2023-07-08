1943a26c45b9ab6200611cc887c84ee34a1dd3d1
Arjun Jhukal <jhukal9@gmail.com>
2023-07-08 10:02:50 +0545

    Try 3 working with netlify form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -16,8 +16,8 @@ const Contact = () => {
         />
         <div className="row justify-content-between">
           <div className="col-lg-6 col-12 order-lg-1 order-2">
-            <form className="contact__form" name="contact" method="POST" data-netlify={true} onSubmit={"submit"}>
-              <input type="hidden" name="subject" value="New lead from %{formName} (%{submissionId})" />
+            <form className="contact__form" name="contact" method="POST" action="/" data-netlify={true} onSubmit={"submit"}>
+              <input type="hidden" name="subject" value="New lead from %{contact} " />
               <div className="row">
                 <div className="col-12">
                   <InputField fieldType={"input"} name={"name"} inputType={"text"} placeholder={"Eg. Arjun Jhukal"} label={"Name"} />
